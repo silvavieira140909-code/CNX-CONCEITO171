5a92057d8de4de4d1c4c3c1da42d9833cdc29a9e
silvavieira140909-code <silvavieira140909@gmail.com>
2026-09-11 05:27:10 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,1 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react';
-export default defineConfig({plugins:[react()]});
+Import defineConfig from Vite e import react from plugin react
